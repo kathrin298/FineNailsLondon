b1686ef4296cfe7d2fff60fb6ef17f6a54b8a5cc
Kathrin Wendt <kathrinwendt2908@gmail.com>
2020-04-30 17:59:24 +0200

    configured default url to set up mailchimp API connection

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,8 @@
+Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+
 Rails.application.configure do
+  # config.action_controller.default_url_options = { host: 'localhost', port: 3000 }
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
