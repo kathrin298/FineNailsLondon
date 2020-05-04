891ee0fac1e34955c67d388ede8f5ac9c754c129
Kathrin Wendt <kathrinwendt2908@gmail.com>
2020-05-04 18:04:27 +0200

    added line to prepend home_controller_decorator to home_controller

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -1,8 +1,10 @@
 # require 'spree/base_controller'
 module Spree
-  Spree::HomeController.class_eval do
+  HomeController.class_eval do
     def contact
       @contact = Contact.new(params[:contact])
     end
   end
 end
+
+Spree::HomeController.prepend Spree::HomeControllerDecorator
