9ba313b6c06e869a93988fc028806a58be1f1686
Kathrin Wendt <kathrinwendt2908@gmail.com>
2020-05-04 17:47:37 +0200

    added Spree module to home_controller_decorator

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -1,7 +1,8 @@
 # require 'spree/base_controller'
-
-Spree::HomeController.class_eval do
-  def contact
-    @contact = Contact.new(params[:contact])
+module Spree
+  Spree::HomeController.class_eval do
+    def contact
+      @contact = Contact.new(params[:contact])
+    end
   end
 end
