a11730082f20d09ac12f3da86c12eebd0f4f295a
Kathrin Wendt <kathrinwendt2908@gmail.com>
2020-05-05 16:18:22 +0200

    changed email back to fines email

--- app/models/spree/contact.rb
+++ app/models/spree/contact.rb
@@ -9,7 +9,7 @@ module Spree
     def headers
       {
         :subject => "Contact Form Inquiry",
-        :to => "kathrinwendt2908@gmail.com",
+        :to => "finenailslondon@gmail.com",
         :from => %("#{name}" <#{email}>)
       }
     end
